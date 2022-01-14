c04b8e537b7b88a4b188c704d883570454e543da
Andraž P <atletek@gmail.com>
2022-01-14 08:37:37 +0100

    Added missing field to backup.

--- backup/moodle2/backup_booking_stepslib.php
+++ backup/moodle2/backup_booking_stepslib.php
@@ -51,7 +51,8 @@ class backup_booking_activity_structure_step extends backup_activity_structure_s
                     'assesstimestart', 'assesstimefinish', 'scale', 'enablepresence',
                     'responsesfields', 'reportfields', 'beforebookedtext', 'beforecompletedtext',
                     'aftercompletedtext', 'signinsheetfields', 'comments', 'ratings', 'removeuseronunenrol',
-                    'teacherroleid', 'allowupdatedays', 'templateid', 'defaultoptionsort', 'showviews'));
+                    'teacherroleid', 'allowupdatedays', 'templateid', 'defaultoptionsort', 'showviews', 
+                    'maxperuseronlyactive'));
 
         $options = new backup_nested_element('options');
         $option = new backup_nested_element('option', array('id'),
