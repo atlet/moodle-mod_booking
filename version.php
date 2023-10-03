1e4c94b48406cf92e2037de1e4a8376d0d82cc16
atlet <atletek@gmail.com>
2023-10-03 13:58:40 +0200

    Dodano preverjanje, da zahteva nameščeno zadnjo verzijo vtičnika tool_certificate.

--- version.php
+++ version.php
@@ -23,11 +23,12 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023091901;
+$plugin->version = 2023100300;
 $plugin->requires = 2022041900; // Requires this Moodle version. Current: Moodle 4.0.0.
 $plugin->release = '8.0.33';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'mod_booking';
 $plugin->dependencies = [
-    'local_wunderbyte_table' => 2023081100
+    'local_wunderbyte_table' => 2023081100,
+    'tool_certificate' => 2023053000
 ];
